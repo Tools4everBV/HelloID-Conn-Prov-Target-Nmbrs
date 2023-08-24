ae582c287cd292a7da595ade1fefd6e6c189bc96
Jerry Breek <j.breek@tools4ever.com>
2023-08-24 13:47:51 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # HelloID-Conn-Prov-Target-Nmbrs
+Repository for HelloID Provisioning Target Connector Nmbrs
+
+
+| :information_source: Information                                                         |
+| :--------------------------------------------------------------------------------------- |
+| This repository contains the connector and configuration code only. The implementer is responsible to acquire the connection details such as username, password, certificate, etc. You might even need to sign a contract or agreement with the supplier before implementing this connector. Please contact the client's application manager to coordinate the connector requirements.       |
+
+<br /> 
+<p align="center">
+  <img src="https://www.tools4ever.nl/connector-logos/vismanmbrs-logo.png" width="500">
+</p>
+
+## Versioning
+| Version | Description     |
+| ------- | --------------- |
+| 1.0.0   | Initial release |
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+- [HelloID-Conn-Prov-Target-Nmbrs](#helloid-conn-prov-target-nmbrs)
+  - [Versioning](#versioning)
+  - [Table of Contents](#table-of-contents)
+  - [Requirements](#requirements)
+  - [Introduction](#introduction)
+  - [Getting Started](#getting-started)
+    - [Application Registration](#application-registration)
+    - [Configuring App Permissions](#configuring-app-permissions)
+    - [Connection settings](#connection-settings)
+  - [Remarks](#remarks)
+  - [Getting help](#getting-help)
+  - [HelloID Docs](#helloid-docs)
+
+## Requirements
+There are no specific requirements besides the necessary connection settings.
+
+## Introduction
+_HelloID-Conn-Prov-Target-NMBRS_ is a _target_ connector. NMBRS provides a set of SOAP API's that allow you to programmatically interact with it's data. The HelloID connector uses the API endpoints listed in the table below.
+
+| Endpoint     | Description |
+| ------------ | ----------- |
+| https://api.nmbrs.nl/soap/v3/EmployeeService.asmx? | info regarding persons, contracts and so on           |
+
+
+With this connector you can add, update and delete employee information within NMBRS. The current code only provides the possibility to update the business e-mailaddress of an employee.
+
+| Action | Action(s) Performed | Comment |
+| ------ | ------------------- | ------- |
+| create.ps1                | Correlate or Correlate-Update employee                                              | Users are only correlated or correlated and updated when this is configured, **make sure to check your configuration options to set the right option**. |
+| update.ps1                | Update account                                                        | Update the e-mailaddress.   |
+| delete.ps1                | Remove account                                                        | Updates the e-mailaddress in NMBRS with an empty value.              |
+
+## Getting Started
+
+### Connection settings
+The following settings are required to connect to the API.
+
+| Setting      | Description                        | Mandatory   |
+| ------------ | -----------                        | ----------- |
+| UserName     | The UserName to connect to the API | Yes         |
+| Token        | The token to connect to the API | Yes         |
+| Domain      | The Domain [mydomain.nmbrs.nl] to connect to the API                | Yes         |
+| BaseUrl | The URL to the API.[https://api.nmbrs.nl] | Yes |
+| Version | The version of the API [v3]               | Yes |
+| CompanyId | The companyId for which the employees will be imported | Yes |
+|proxyAddress| The addres of the proxy  |No |
+|IsDebug | When toggled, debug logging will be displayed |
+| updateOnCorrelate | When toggled, employee will be updated on correlation | 
+
+
+## Remarks
+Some fields are mandatory to provide to the NMBRS api to perform updates. The value for these fields will be used from the employee data which is returned from the NMBRS api when looking up the employee.
+
+## Getting help
+> _For more information on how to configure a HelloID PowerShell connector, please refer to our [documentation](https://docs.helloid.com/hc/en-us/articles/360012518799-How-to-add-a-target-system) pages_
+
+> _If you need help, feel free to ask questions on our [TODO-forum](https://forum.helloid.com/forum/helloid-connectors/provisioning/0000-helloid-provisioning-helloid-conn-prov-target-nmbrs)_
+
+## HelloID Docs
+The official HelloID documentation can be found at: https://docs.helloid.com/
---
+++ delete.ps1
@@ -0,0 +1,398 @@
+#####################################################
+# HelloID-Conn-Prov-Target-Nmbrs-Delete
+#
+# Version: 1.0.0
+#####################################################
+# Initialize default values
+$c = $configuration | ConvertFrom-Json
+$p = $person | ConvertFrom-Json
+$aRef = $accountReference | ConvertFrom-Json
+$success = $false # Set to false at start, at the end, only when no error occurs it is set to true
+$auditLogs = [System.Collections.Generic.List[PSCustomObject]]::new()
+
+# Set TLS to accept TLS, TLS 1.1 and TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+
+# Set debug logging
+switch ($($c.isDebug)) {
+    $true { $VerbosePreference = "Continue" }
+    $false { $VerbosePreference = "SilentlyContinue" }
+}
+$InformationPreference = "Continue"
+$WarningPreference = "Continue"
+
+# Define configuration properties as required
+$requiredConfigurationFields = @("BaseUrl", "UserName", "Token", "Domain", "version")
+
+# Correlation values
+$correlationProperty = "Id" # Has to match the name of the unique identifier
+$correlationValue = $p.externalId # Has to match the value of the unique identifier
+
+# Change mapping here - Empty as we delete the account
+$account = [PSCustomObject]@{
+    Id  = $p.ExternalId    
+}
+
+# Define account properties as required - Empty as we delete the account
+$requiredAccountFields = @("Id")
+
+# Define account properties to update - Empty as we delete the account
+#$updateAccountFields = @()
+
+# Define account properties to store in account data - Empty as we delete the account
+#$storeAccountFields = @()
+
+#region functions
+function Resolve-HTTPError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+            ValueFromPipeline
+        )]
+        [object]$ErrorObject
+    )
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            FullyQualifiedErrorId = $ErrorObject.FullyQualifiedErrorId
+            MyCommand             = $ErrorObject.InvocationInfo.MyCommand
+            RequestUri            = $ErrorObject.TargetObject.RequestUri
+            ScriptStackTrace      = $ErrorObject.ScriptStackTrace
+            ErrorMessage          = ""
+        }
+        if ($ErrorObject.Exception.GetType().FullName -eq "Microsoft.PowerShell.Commands.HttpResponseException") {
+            $httpErrorObj.ErrorMessage = $ErrorObject.ErrorDetails.Message
+        }
+        elseif ($ErrorObject.Exception.GetType().FullName -eq "System.Net.WebException") {
+            $httpErrorObj.ErrorMessage = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
+        }
+        Write-Output $httpErrorObj
+    }
+}
+
+function Get-ErrorMessage {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+            ValueFromPipeline
+        )]
+        [object]$ErrorObject
+    )
+    process {
+        $errorMessage = [PSCustomObject]@{
+            VerboseErrorMessage = $null
+            AuditErrorMessage   = $null
+        }
+
+        if ( $($ErrorObject.Exception.GetType().FullName -eq "Microsoft.PowerShell.Commands.HttpResponseException") -or $($ErrorObject.Exception.GetType().FullName -eq "System.Net.WebException")) {
+            $httpErrorObject = Resolve-HTTPError -Error $ErrorObject
+
+            $errorMessage.VerboseErrorMessage = $httpErrorObject.ErrorMessage
+
+            $errorMessage.AuditErrorMessage = $httpErrorObject.ErrorMessage
+        }
+
+        # If error message empty, fall back on $ex.Exception.Message
+        if ([String]::IsNullOrEmpty($errorMessage.VerboseErrorMessage)) {
+            $errorMessage.VerboseErrorMessage = $ErrorObject.Exception.Message
+        }
+        if ([String]::IsNullOrEmpty($errorMessage.AuditErrorMessage)) {
+            $errorMessage.AuditErrorMessage = $ErrorObject.Exception.Message
+        }
+
+        Write-Output $errorMessage
+    }
+}
+
+function Invoke-NMBRSRestMethod {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Uri,
+
+        [Parameter(Mandatory)]
+        [string]
+        $Service,
+
+        [Parameter(Mandatory)]
+        [string]
+        $SoapBody
+    )
+
+    switch ($service) {
+        'EmployeeService' {
+            $soapHeader = "
+            <emp:AuthHeaderWithDomain>
+                <emp:Username>$($c.UserName)</emp:Username>
+                <emp:Token>$($c.Token)</emp:Token>
+                <emp:Domain>$($c.Domain)</emp:Domain>
+            </emp:AuthHeaderWithDomain>"
+        }
+    }
+
+    $xmlRequest = "<?xml version=`"1.0`" encoding=`"utf-8`"?>
+        <soap:Envelope xmlns:soap= `"http://www.w3.org/2003/05/soap-envelope`" xmlns:emp=`"https://api.nmbrs.nl/soap/$($c.version)/$service`">
+        <soap:Header>
+            $soapHeader
+        </soap:Header>
+        <soap:Body>
+            $soapBody
+        </soap:Body>
+        </soap:Envelope>"
+
+    try {
+        $splatParams = @{
+            Uri         = $Uri
+            Method      = 'POST'
+            Body        = $xmlRequest
+            ContentType = 'text/xml; charset=utf-8'
+        }
+
+        if (-not  [string]::IsNullOrEmpty($c.ProxyAddress)) {
+            $splatParams['Proxy'] = $c.ProxyAddress
+
+        }
+        #Invoke-WebRequest @splatParams
+        Invoke-RestMethod @splatParams -Verbose:$false
+    }
+    catch {
+        $PSCmdlet.ThrowTerminatingError($_)
+    }
+}
+
+function Get-CurrentPersonalInfo {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $EmployeeId
+    )
+
+    $splatParams = @{
+        Uri      = "$($c.BaseUrl)/soap/$($c.version)/EmployeeService.asmx"
+        Service  = 'EmployeeService'
+        SoapBody = "<emp:PersonalInfo_GetCurrent xmlns=`"https://api.nmbrs.nl/soap/$($c.version)/EmployeeService`">
+            <emp:EmployeeId>$EmployeeId</emp:EmployeeId>
+            </emp:PersonalInfo_GetCurrent>"
+    }
+    try {
+        [xml]$response = Invoke-NMBRSRestMethod @splatParams
+        Write-Output $response.Envelope.Body.PersonalInfo_GetCurrentResponse.PersonalInfo_GetCurrentResult
+    }
+    catch {
+        $ex = $PSItem
+        $errorMessage = Get-ErrorMessage -ErrorObject $ex
+
+        Write-Verbose "Error at Line [$($ex.InvocationInfo.ScriptLineNumber)]: $($ex.InvocationInfo.Line). Error: $($errorMessage.VerboseErrorMessage) [$($ex.ErrorDetails.Message)]"
+                
+        $auditLogs.Add([PSCustomObject]@{
+                # Action  = "" # Optional
+                Message = "Error updating account [$($p.DisplayName) ($($currentAccount.Id))]. Error Message: $($errorMessage.AuditErrorMessage). Account object: $($updateAccountObject | ConvertTo-Json -Depth 10)"
+                IsError = $true
+            })
+        break
+    }
+}
+
+function Set-CurrentPersonalInfo {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $EmployeeId,
+
+        [Parameter(Mandatory)]
+        [string]
+        $EmployeeBody
+    )
+
+    $splatParams = @{
+        Uri      = "$($c.BaseUrl)/soap/$($c.version)/EmployeeService.asmx"
+        Service  = 'EmployeeService'
+        SoapBody = "<emp:PersonalInfo_UpdateCurrent xmlns=`"https://api.nmbrs.nl/soap/$($c.version)/EmployeeService`">
+            <emp:EmployeeId>$EmployeeId</emp:EmployeeId>
+            <emp:PersonalInfo>$EmployeeBody</emp:PersonalInfo>
+            </emp:PersonalInfo_UpdateCurrent>"
+    }
+
+    try {
+        [xml]$response = Invoke-NMBRSRestMethod @splatParams
+        Write-Output $response.Envelope.Body.PersonalInfo_UpdateCurrentResponse
+    }
+    catch {
+        $ex = $PSItem
+        $errorMessage = Get-ErrorMessage -ErrorObject $ex
+
+        Write-Verbose "Error at Line [$($ex.InvocationInfo.ScriptLineNumber)]: $($ex.InvocationInfo.Line). Error: $($errorMessage.VerboseErrorMessage) [$($ex.ErrorDetails.Message)]"
+                
+        $auditLogs.Add([PSCustomObject]@{
+                # Action  = "" # Optional
+                Message = "Error updating account [$($p.DisplayName) ($($currentAccount.Id))]. Error Message: $($errorMessage.AuditErrorMessage). Account object: $($updateAccountObject | ConvertTo-Json -Depth 10)"
+                IsError = $true
+            })
+        break
+    }
+}
+#endregion functions
+
+try {
+    # Check if required fields are available in configuration object
+    $incompleteConfiguration = $false
+    foreach ($requiredConfigurationField in $requiredConfigurationFields) {
+        if ($requiredConfigurationField -notin $c.PsObject.Properties.Name) {
+            $incompleteConfiguration = $true
+            Write-Warning "Required configuration object field [$requiredConfigurationField] is missing"
+        }
+        elseif ([String]::IsNullOrEmpty($c.$requiredConfigurationField)) {
+            $incompleteConfiguration = $true
+            Write-Warning "Required configuration object field [$requiredConfigurationField] has a null or empty value"
+        }
+    }
+
+    if ($incompleteConfiguration -eq $true) {
+        throw "Configuration object incomplete, cannot continue."
+    }
+
+    # Check if required fields are available for correlation
+    $incompleteCorrelationValues = $false
+    if ([String]::IsNullOrEmpty($correlationProperty)) {
+        $incompleteCorrelationValues = $true
+        Write-Warning "Required correlation field [$correlationProperty] has a null or empty value"
+    }
+    if ([String]::IsNullOrEmpty($correlationValue)) {
+        $incompleteCorrelationValues = $true
+        Write-Warning "Required correlation field [$correlationValue] has a null or empty value"
+    }
+    
+    if ($incompleteCorrelationValues -eq $true) {
+        throw "Correlation values incomplete, cannot continue. CorrelationProperty = [$correlationProperty], CorrelationValue = [$correlationValue]"
+    }
+
+    # Check if required fields are available in account object
+    $incompleteAccount = $false
+    foreach ($requiredAccountField in $requiredAccountFields) {
+        if ($requiredAccountField -notin $account.PsObject.Properties.Name) {
+            $incompleteAccount = $true
+            Write-Warning "Required account object field [$requiredAccountField] is missing"
+        }
+        elseif ([String]::IsNullOrEmpty($account.$requiredAccountField)) {
+            $incompleteAccount = $true
+            Write-Warning "Required account object field [$requiredAccountField] has a null or empty value"
+        }
+    }
+
+    if ($incompleteAccount -eq $true) {
+        throw "Account object incomplete, cannot continue."
+    }
+
+    
+    # Get current account and verify if there are changes
+    try {
+        Write-Verbose "Querying account where [$($correlationProperty)] = [$($correlationValue)]"
+
+        $currentAccount = $null
+        $currentAccount = Get-CurrentPersonalInfo -EmployeeID $($account.Id)
+
+        if ($null -eq $currentAccount) {
+            if ($($c.createUser) -eq $true) {
+                Write-Verbose "No account found where  [$($correlationProperty)] = [$($correlationValue)]. Creating new account"
+                $auditLogs.Add([PSCustomObject]@{
+                        # Action  = "" # Optional
+                        Message = "No account found where [$($correlationProperty)] = [$($correlationValue)] and no account will be created with this connector!"
+                        IsError = $true
+                    })
+            }  
+        }
+    }
+    catch {
+        $ex = $PSItem
+        $errorMessage = Get-ErrorMessage -ErrorObject $ex
+
+        Write-Verbose "Error at Line [$($ex.InvocationInfo.ScriptLineNumber)]: $($ex.InvocationInfo.Line). Error: $($errorMessage.VerboseErrorMessage)"
+
+        if ($errorMessage.AuditErrorMessage -Like "No account found where [$($correlationProperty)] = [$($correlationValue)]") {
+            $auditLogs.Add([PSCustomObject]@{
+                    # Action  = "" # Optional
+                    Message = "No account found where [$($correlationProperty)] = [$($correlationValue)]. Possibly already deleted, skipped action."
+                    IsError = $false
+                })
+        }
+        else {
+            $auditLogs.Add([PSCustomObject]@{
+                    # Action  = "" # Optional
+                    Message = "Error querying account where [$($correlationProperty)] = [$($correlationValue)]. Error Message: $($errorMessage.AuditErrorMessage)"
+                    IsError = $true
+                })
+        }
+
+        # Skip further actions, as this is a critical error
+        continue
+    }
+
+    # Delete account
+    try {
+        $deleteAccountObject = @{                            
+            Id             = $currentAccount.Id             #Mandatory Field within Set-CurrentPerson
+            Number         = $currentAccount.Number         #Mandatory Field within Set-CurrentPerson
+            EmployeeNumber = $currentAccount.EmployeeNumber #Mandatory Field within Set-CurrentPerson
+            LastName       = $currentAccount.LastName       #Mandatory Field within Set-CurrentPerson
+            EmailWork      = ""                             #Field to clear
+            Birthday       = $currentAccount.Birthday       #Mandatory Field within Set-CurrentPerson
+        }
+
+        $body = $null
+
+        foreach ($property in $deleteAccountObject.GetEnumerator()) {
+            $body += "<emp:$($property.Name)>$($property.Value)</emp:$($property.Name)>"
+        }
+
+        Write-Verbose "Deleting account [$($p.DisplayName) ($($currentAccount.Id))]"
+                        
+        if (-not($dryRun -eq $true)) {
+            $null = Set-CurrentPersonalInfo -EmployeeId $($currentAccount.Id) -EmployeeBody $body
+
+            $auditLogs.Add([PSCustomObject]@{
+                    # Action  = "" # Optional
+                    Message = "Successfully deleted account [$($p.DisplayName) ($($currentAccount.Id))]"
+                    IsError = $false
+                })
+        }
+        else {
+            Write-Warning "DryRun: Would delete account [$($p.DisplayName) ($($currentAccount.Id))]"
+        }
+
+        break
+    }
+    catch {
+        $ex = $PSItem
+        $errorMessage = Get-ErrorMessage -ErrorObject $ex
+                
+        Write-Verbose "Error at Line [$($ex.InvocationInfo.ScriptLineNumber)]: $($ex.InvocationInfo.Line). Error: $($errorMessage.VerboseErrorMessage)"
+            
+        $auditLogs.Add([PSCustomObject]@{
+                # Action  = "" # Optional
+                Message = "Error deleting account [$($p.DisplayName) ($($currentAccount.Id))]. Error Message: $($errorMessage.AuditErrorMessage)"
+                IsError = $true
+            })
+    }
+}
+finally {
+    # Check if auditLogs contains errors, if no errors are found, set success to true
+    if (-NOT($auditLogs.IsError -contains $true)) {
+        $success = $true
+    }
+    
+    # Send results
+    $result = [PSCustomObject]@{
+        Success          = $success
+        AccountReference = $aRef
+        AuditLogs        = $auditLogs
+        PreviousAccount  = $previousAccount
+        Account          = $account
+    
+        # Optionally return data for use in other systems
+        ExportData       = $exportData
+    }
+    
+    Write-Output ($result | ConvertTo-Json -Depth 10)  
+}
